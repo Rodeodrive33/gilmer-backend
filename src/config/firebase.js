710fa4f36b8fefa52f81304a341119a7b5e3c68b
Rodeodrive33 <rooney.wanjohi@strathmore.edu>
2026-02-09 10:59:12 +0300

    Save current changes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,20 +1,11 @@
-// src/config/firebase.js
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
 
-const root = path.resolve(process.cwd());
-const serviceAccountPath = path.join(root, "serviceAccountKey.json");
-
-// Read JSON file (avoid import assert issues)
-if (!admin.apps.length) {
-  const raw = fs.readFileSync(serviceAccountPath, "utf8");
-  const serviceAccount = JSON.parse(raw);
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-  console.log("🔥 Firebase Admin initialized successfully");
-}
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+});
 
 export default admin;
-
